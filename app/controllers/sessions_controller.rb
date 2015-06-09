78f45d5771777d5f7b2e562953026bba694c45fb
Carlos Carrera <crlc@me.com>
2015-06-09 15:02:21 -0700

    Deploy Auth with BCrypt

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    )
+    if @user
+      sign_in(@user)
+      redirect_to root_url
+    else
+      flash.now[:errors] = ['Invalid Username or Password']
+      render :new
+    end
+  end
+
+  def destroy
+    signout
+    redirect_to new_session_url
+  end
+end
