d76837fee59c1505ffebcc7c0b9ab8e5574f8018
Carlos Carrera <crlc@me.com>
2015-06-13 16:37:12 -0700

    Add post show modal

---
+++ app/assets/javascripts/views/posts/post_modal.js
@@ -0,0 +1,37 @@
+Yeehaw.Views.PostModal = Backbone.CompositeView.extend({
+  template: JST['posts/modal'],
+
+  events: {
+    'click .del': 'destroyPost',
+    'click .close': 'dismiss',
+    'click .m-backdrop' : 'dismiss'
+  },
+
+  initialize: function () {
+    this.model.fetch();
+    this.listenTo(this.model, 'sync', this.render);
+    this.replyListView = new Yeehaw.Views.RepliesList({
+      collection: this.model.replies()
+    });
+
+    this.addSubview('.post-modal-replies', this.replyListView);
+  },
+
+  destroyPost: function (event) {
+    event.preventDefault();
+    this.model.destroy();
+    this.remove();
+  },
+
+  dismiss: function (event) {
+    event.preventDefault();
+    this.remove();
+  },
+
+  render: function () {
+    var renderedContent = this.template({ post: this.model });
+    this.$el.html(renderedContent);
+    this.attachSubviews();
+    return this;
+  }
+});
--- app/views/api/posts/show.json.jbuilder
+++ app/views/api/posts/show.json.jbuilder
@@ -1,7 +1,7 @@
-json.extract! @post, :id, :handle, :body, :created_at, :updated_at
+json.extract! @post, :id, :handle, :body, :author_id, :created_at, :updated_at
 json.time_ago time_ago_in_words(@post.created_at)
 
 json.replies @post.replies do |reply|
-  json.extract! reply, :id, :post_id, :body, :created_at, :updated_at
+  json.extract! reply, :id, :post_id, :body, :author_id, :created_at, :updated_at
   json.time_ago time_ago_in_words(reply.created_at)
 end
