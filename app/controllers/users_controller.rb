1adadc8b31011af9936c90e7ccca151b8c058fe2
Carlos Carrera <crlc@me.com>
2015-06-15 15:14:50 -0700

    Add guest login and stylize pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      @user.followings.create(follower_id: @user.id, group_id: 1)
       sign_in(@user)
       redirect_to root_url
     else
