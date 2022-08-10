2aff1a495340df117ecd2ac8c022f2555dd2b556
Carlos Carrera <crlc@me.com>
2022-08-09 22:34:16 -0400

    Fix reply belongs_to

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -3,6 +3,6 @@ class Reply < ApplicationRecord
   validates :body, length: { maximum: 200 }
 
   belongs_to :author, class_name: 'User'
-  belongs_to :posts
+  belongs_to :posts, optional: true
   acts_as_votable
 end
