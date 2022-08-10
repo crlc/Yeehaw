9be5b2f24d0ed0c822ff1adb8312db2e432fdab9
Carlos Carrera <crlc@me.com>
2022-08-09 22:00:09 -0400

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,27 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    root 'static_pages#root'
+
+   resources :users, only: [:create, :new]
+   resource :session, only: [:create, :new, :destroy]
+
+   namespace :api, defaults: { format: :json } do
+     resources :followings, only: [:create, :destroy]
+     resources :groups, except: [:new, :edit]
+     resources :posts, except: [:new, :edit] do
+       member do
+         post 'like',    to: 'posts#upvote'
+         post 'dislike', to: 'posts#downvote'
+         post 'unlike',    to: 'posts#unupvote'
+         post 'undislike', to: 'posts#undownvote'
+       end
+     end
+     resources :replies, except: [:new, :edit] do
+       member do
+         post 'like',    to: 'replies#upvote'
+         post 'dislike', to: 'replies#downvote'
+         post 'unlike',    to: 'replies#unupvote'
+         post 'undislike', to: 'replies#undownvote'
+       end
+     end
+   end
 end
